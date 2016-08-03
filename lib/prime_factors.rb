9931b4bb45a746820168edc3d01ad191c6eb077c
adammartin <ebondark.od@gmail.com>
2016-08-03 08:08:45 -0500

    fourth passing test

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -2,7 +2,11 @@ class Primes
 	def factors number
 		primes = []
 		return primes unless number > 1
-		primes << number
+		while number % 2 == 0 do
+			number /= 2
+			primes << 2
+		end
+		primes << number if number > 1
 		primes
 	end
 end
