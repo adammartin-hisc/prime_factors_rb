944e4bd5030b38ad18e1f8e9842f3b49e568b557
adammartin <ebondark.od@gmail.com>
2016-08-03 07:57:45 -0500

    Project initialized

---
+++ lib/prime_factors.rb
@@ -0,0 +1,2 @@
+class Primes
+end
---
+++ spec/prime_factors_spec.rb
@@ -0,0 +1,9 @@
+require 'prime_factors'
+
+describe Primes do
+	let(:primes) { Primes.new }
+	
+	it 'returns [] for the primes of 1' do
+		expect(true).to eq false
+	end
+end
