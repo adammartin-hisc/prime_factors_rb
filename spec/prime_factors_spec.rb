8ea8734d3b6f3c5f0c128293fcc4b7fe5f643930
adammartin <ebondark.od@gmail.com>
2016-08-03 08:02:35 -0500

    first failing test

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -2,8 +2,8 @@ require 'prime_factors'
 
 describe Primes do
 	let(:primes) { Primes.new }
-	
+
 	it 'returns [] for the primes of 1' do
-		expect(true).to eq false
+		expect(primes.factors 1).to eq []
 	end
 end
