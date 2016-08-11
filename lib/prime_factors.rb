c7dfa5466f7df66cc5cdd7189976f76b84596842
adammartin <ebondark.od@gmail.com>
2016-08-11 16:21:49 -0500

    Kata not completed :( ... was really excited.  Took advice well though

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,2 +1,24 @@
 class Primes
+
+	def initialize(n)
+		@n = n
+	end
+
+	def determine_primes
+		prime_factors = []
+
+		mod_num = 2
+
+		until @n <= 1 
+			while @n % mod_num == 0
+				prime_factors << mod_num
+				@n = @n / mod_num
+			end
+			mod_num = mod_num + 1
+		end
+
+		prime_factors
+	end
+
+
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -1,9 +1,31 @@
 require 'prime_factors'
 
 describe Primes do
-	let(:primes) { Primes.new }
 	
-	it 'returns [] for the primes of 1' do
-		expect(true).to eq false
+	it 'returns [2] if given input of 2' do
+		calc = Primes.new(2)
+
+		expect(calc.determine_primes).to eq([2])
 	end
+
+	it 'returns [3] if given input of 3' do 
+		calc = Primes.new(3)
+
+		expect(calc.determine_primes).to eq([3])
+	end
+
+	it 'returns [2, 2] if given input of 4' do 
+		calc = Primes.new(4)
+
+		expect(calc.determine_primes).to eq([2,2])
+	end
+
+
+	it 'returns [5] if given input of 5' do 
+		calc = Primes.new(5)
+
+		expect(calc.determine_primes).to eq([5])
+	end
+
+
 end
