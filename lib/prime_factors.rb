7702c0a9e33be327a2315c39fb40d17992f7ced8
adammartin <ebondark.od@gmail.com>
2016-08-17 14:32:57 -0500

    Done in text book style ... clearly practiced

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,2 +1,10 @@
 class Primes
+	def of(num)
+		factors, candidate = [], 1
+		while num > 1 and candidate += 1
+			factors << candidate and num /= candidate while num % candidate == 0
+			
+		end
+		factors
+	end
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -3,7 +3,23 @@ require 'prime_factors'
 describe Primes do
 	let(:primes) { Primes.new }
 	
-	it 'returns [] for the primes of 1' do
-		expect(true).to eq false
+	
+	[
+		[1, []],
+		[2, [2]],
+		[3, [3]],
+		[4, [2, 2]],
+		[5, [5]],
+		[6, [2, 3]],
+		[7, [7]],
+		[8, [2, 2, 2]],
+		[9, [3, 3]],
+		[10, [2, 5]],
+		[2*3*5*7*11*13, [2, 3, 5, 7, 11, 13]]
+	].each do |num, factors|
+
+		it 'returns #{factors} for the primes of #{num}' do
+			expect(primes.of(num)).to eq factors
+		end
 	end
 end
